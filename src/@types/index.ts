c725fd4fb48f1f8d98cc0dff58899fed317083a7
Parksangwoo <psu8430@gmail.com>
2023-05-16 08:24:32 +0900

    feat: type 관련 파일 추가

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,1 +1,25 @@
-export {};
+export interface TodoType {
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+  id: string;
+}
+
+export interface TodoRequestDataType {
+  title: string;
+}
+
+export interface SearchRequestType {
+  q: string;
+  page?: number;
+  limit?: number;
+}
+
+export interface SearchResponseType {
+  q: string;
+  page: number;
+  limit: number;
+  result: string[];
+  qty: number;
+  total: number;
+}
