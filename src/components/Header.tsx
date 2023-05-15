733907dd6cf93d1038ada76f8b69c3e4db8a5a28
Parksangwoo <psu8430@gmail.com>
2023-05-16 01:22:25 +0900

    feat: Header component 추가

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { headerStyle, titleStyle } from '../style/headerStyle';
+
+const Header = () => {
+  return (
+    <header style={headerStyle}>
+      <h1 style={titleStyle}>Toodos</h1>
+    </header>
+  );
+};
+
+export default Header;
